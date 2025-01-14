531151e1a35dbade6578e6e4d1ac4a895b5b26f8
porrt23 <71556442+porrt23@users.noreply.github.com>
2025-01-13 21:34:25 -0500

    wind test

--- pages/01_Wind.py
+++ pages/01_Wind.py
@@ -2,7 +2,7 @@ from math import sqrt
 from handcalcs.decorator import handcalc
 import streamlit as st
 
-@handcalc
+@handcalc()
 def quadratic(a,b,c):
     x_1 = (-b + sqrt(b**2 - 4*a*c)) / (2*a)
     x_2 = (-b - sqrt(b**2 - 4*a*c)) / (2*a)
