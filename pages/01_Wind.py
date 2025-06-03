187f94ad11d6c7b7faabd66d21eab4ac334e5b23
porrt23 <71556442+porrt23@users.noreply.github.com>
2025-06-02 21:00:36 -0400

    class test

--- pages/01_Wind.py
+++ pages/01_Wind.py
@@ -11,18 +11,31 @@ import streamlit as st
 # result, latex = my_calc()
 
 # @handcalc()
-@handcalc(override='latex', jupyter_display=False)
-def quadratic(a,b,c):
-    x_1 = (-b + sqrt(b**2 - 4*a*c)) / (2*a)
-    x_5 = (-b - sqrt(b**2 - 4*a*c)) / (2*a)
-    return locals()
+# @handcalc(override='latex', jupyter_display=False)
+# def quadratic(a,b,c):
+#     x_1 = (-b + sqrt(b**2 - 4*a*c)) / (2*a)
+#     x_5 = (-b - sqrt(b**2 - 4*a*c)) / (2*a)
+#     return locals()
 
-a = st.slider("Value for a:", 1, 5, 5)
-b = st.slider("Value for b:", -10, 10, -5)
-c = st.slider("Value for c:", -20, 0, -5)
+# a = st.slider("Value for a:", 1, 5, 5)
+# b = st.slider("Value for b:", -10, 10, -5)
+# c = st.slider("Value for c:", -20, 0, -5)
 
-st.write("Quadratic equation in x:")
-st.latex(f"{a}x^2 + {b}x + {c} = 0")
-latex_code, vals = quadratic(a,b,c)
-st.write(f"Latex={latex_code}")
-st.latex(latex_code)
+# st.write("Quadratic equation in x:")
+# st.latex(f"{a}x^2 + {b}x + {c} = 0")
+# latex_code, vals = quadratic(a,b,c)
+# st.write(f"Latex={latex_code}")
+# st.latex(latex_code)
+
+
+class MyClass:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    @handcalc(override='latex', jupyter_display=False)
+    def my_method(self):
+        a = self.a  # Assign instance to local variable
+        b = self.b
+        c = a + b
+        return c
