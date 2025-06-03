84f8c1920fc90586e9e42e48c7908fe2f4f36316
porrt23 <71556442+porrt23@users.noreply.github.com>
2025-06-02 20:36:46 -0400

    handcalc override

--- pages/01_Wind.py
+++ pages/01_Wind.py
@@ -2,12 +2,20 @@ from math import sqrt
 from handcalcs.decorator import handcalc
 import streamlit as st
 
+# @handcalc(override='latex', jupyter_display=False)
+# def my_calc():
+#     a = 2
+#     b = 3
+#     c = 2*a + b/3
+#     return c
+# result, latex = my_calc()
+
+# @handcalc()
 @handcalc(override='latex', jupyter_display=False)
-def my_calc():
-    a = 2
-    b = 3
-    c = 2*a + b/3
-    return c
+def quadratic(a,b,c):
+    x_1 = (-b + sqrt(b**2 - 4*a*c)) / (2*a)
+    x_2 = (-b - sqrt(b**2 - 4*a*c)) / (2*a)
+    return x_1
 
 a = st.slider("Value for a:", 1, 5, 5)
 b = st.slider("Value for b:", -10, 10, -5)
@@ -16,22 +24,5 @@ c = st.slider("Value for c:", -20, 0, -5)
 st.write("Quadratic equation in x:")
 st.latex(f"{a}x^2 + {b}x + {c} = 0")
 
-latex_code, vals = my_calc(a,b,c)
+latex_code, vals = quadratic(a,b,c)
 st.latex(latex_code)
-
-
-# @handcalc()
-# def quadratic(a,b,c):
-#     x_1 = (-b + sqrt(b**2 - 4*a*c)) / (2*a)
-#     x_2 = (-b - sqrt(b**2 - 4*a*c)) / (2*a)
-#     return x_1
-
-# a = st.slider("Value for a:", 1, 5, 5)
-# b = st.slider("Value for b:", -10, 10, -5)
-# c = st.slider("Value for c:", -20, 0, -5)
-
-# st.write("Quadratic equation in x:")
-# st.latex(f"{a}x^2 + {b}x + {c} = 0")
-
-# latex_code, vals = quadratic(a,b,c)
-# st.latex(latex_code)
